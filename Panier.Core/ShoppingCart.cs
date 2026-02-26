84b93a13a46bffa6361164c84b46427562e86c78
Dylandcss <decmps.dylan@gmail.com>
2026-02-26 12:11:26 +0100

    Implémentation méthode GetItemCount pour passer le test

--- Panier.Core/ShoppingCart.cs
+++ Panier.Core/ShoppingCart.cs
@@ -6,7 +6,8 @@ namespace Panier.Core
 {
     public sealed class ShoppingCart
     {
-        public int GetItemCount() => throw new NotImplementedException();
+        private List<CartItem> _items = [];
+        public int GetItemCount() => _items.Where(i => i.Quantity > 0).Sum(i => i.Quantity);
         public void AddItem(string name, decimal price, int quantity) => throw new NotImplementedException();
         public decimal GetTotal() => throw new NotImplementedException();
         public void ApplyDiscount(decimal percentage) => throw new NotImplementedException();
